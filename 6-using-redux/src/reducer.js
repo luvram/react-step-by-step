5e3d73a1533ab7536c07f1d270c0b124595e2fb9
luvram <sahul0804@gmail.com>
2018-06-19 14:50:44 +0900

    6-remove unused variable

--- 6-using-redux/src/reducer.js
+++ 6-using-redux/src/reducer.js
@@ -1,6 +1,6 @@
 import { ADD_COUNT } from './action';
 
-export function calculate(state = { name: 'boram', count: 1 }, action) {
+export function calculate(state = { count: 1 }, action) {
   switch (action.type) {
     case ADD_COUNT:
       const newState = Object.assign({}, state, { count: state.count + action.count });
